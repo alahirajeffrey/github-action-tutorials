1bdc64a3632e18e4e90862b1458b07b863ae265e
alahirajeffrey <alahirajeffrey@gmail.com>
2023-02-23 16:28:50 +0100

    setup test file

---
+++ tests/test.js
@@ -0,0 +1,14 @@
+const {
+  addition,
+  subtraction,
+  division,
+  multiplication,
+} = require("../src/index");
+
+describe("Addition", () => {});
+
+describe("Subtraction", () => {});
+
+describe("Division", () => {});
+
+describe("Multiplication", () => {});
